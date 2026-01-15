d7166482582f46043f14647a998a29e57ffbdd6c
Maria Rahmani <mariaubaidrahmani@gmail.com>
2026-01-15 21:46:31 +0530

    tuple basics

---
+++ module2/tuple1.py
@@ -0,0 +1,23 @@
+# lst = [1, 2, 3]
+# print(id(lst))
+# lst = lst + [8, 9, 10]
+# print(id(lst))
+
+
+# tpl = (1, 2, 3)
+# print(id(tpl))
+# tpl = tpl + (8, 9, 10)
+# print(tpl)
+
+# Difference between list and tuple
+
+# daji = ["Miftah", (100, 20, 300)]
+
+# daji[1][2] = 9999999
+
+# print(daji)
+
+# tpl = tuple(20)
+tpl = (20, )
+
+print(type(tpl))
