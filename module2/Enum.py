19f4d7d6f0a7bff441c7d47f13a5216b96718af6
Maria Rahmani <mariaubaidrahmani@gmail.com>
2026-01-18 00:11:48 +0530

    Enum and Zip Function

--- module2/Enum.py
+++ module2/Enum.py
@@ -1,5 +1,6 @@
 # If we want to keep track of the index of each element of the list, we do 
 x = [8, 3, 2, 9, 4]
+y = ["I", "Love", "You", "Munnu", "Gunnu"]
 index = 0
 
 for i in x:
@@ -8,3 +9,12 @@ for i in x:
 
 # With Enumerate function
 print(list(enumerate(x)))
+print(list(enumerate(x, 5)))
+
+# Using Zip function
+print(list(zip(x, y)))
+
+# Another way to use Zip function
+for Id, name in zip(x, y):
+    print(Id, "->", name)
+
