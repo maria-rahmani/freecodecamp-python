3a6b9974afc8b894eb0c32fa3fd111dd4ee81919
Maria Rahmani <mariaubaidrahmani@gmail.com>
2026-01-17 23:13:59 +0530

    Tracking index

---
+++ module2/Enum.py
@@ -0,0 +1,7 @@
+# If we want to keep track of the index of each element of the list, we do 
+x = [8, 3, 2, 9, 4]
+index = 0
+
+for i in x:
+    print(f'Index {index} has element {i}')
+    index += 1
