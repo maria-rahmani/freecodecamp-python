00649033def606aec9c88a028619e553c461eb04
Maria Rahmani <mariaubaidrahmani@gmail.com>
2026-01-26 00:15:50 +0530

    re module more functions practice

--- module3/reModule.py
+++ module3/reModule.py
@@ -1,5 +1,8 @@
 import re
 
 name = "Maria Rahmani"
-print(re.search('Ma', name))
-print(re.search('me', name))
+#print(re.search('m\d', name, re.IGNORECASE))
+#print(re.search('me', name))
+
+book = "It ends with us 567"
+print(re.search('\d+', book))
